5e4ad0742cf2457c22ab66912bfd5e7856095fdf
volkov <volkov.ivan08.12@gmail.com>
2022-06-17 12:56:59 +0300

    Add empty spring app with default dependencies.

---
+++ src/main/java/zlagoda/server/company/CompanyApplication.java
@@ -0,0 +1,15 @@
+package zlagoda.server.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CompanyApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(CompanyApplication.class, args);
+	}
+}
