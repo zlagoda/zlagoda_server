1088a6b46da0669b34862a5f5dd67e06207de390
Bohdan Tsehelnyk <mail.qewby@gmail.com>
2022-06-19 15:16:57 +0300

    fix: delete jdbc data dependency

--- src/main/java/zlagoda/server/entity/Category.java
+++ src/main/java/zlagoda/server/entity/Category.java
@@ -1,23 +1,15 @@
 package zlagoda.server.entities;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.*;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Table("Category")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
-   @Id
-   @Column("category_number")
    private long id;
-
-   @Column("category_name")
    private String name;
 }
