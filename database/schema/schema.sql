2afc36c8b584aead5b7f2241454b878867c3c842
volkov <volkov.ivan08.12@gmail.com>
2022-07-05 13:30:33 +0300

    commit

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -4,6 +4,7 @@ CREATE TABLE `Employee` (
   `empl_surname` VARCHAR(50) NOT NULL,
   `empl_name` VARCHAR(50) NOT NULL,
   `password` VARCHAR(72) NOT NULL,
+  `role` VARCHAR(15) NOT NULL,
   `empl_patronymic` VARCHAR(50) NULL,
   `salary` DECIMAL(13, 4) NOT NULL CHECK(`salary` > 0),
   `date_of_birth` DATE NOT NULL,
